c4fb49af3314b0f581634ddf63107446ac45690f
Max Zakharzhevskiy <maxzz@msn.com>
2025-11-22 15:48:58 -0800

    WIP. add so on. 3. utils

---
+++ src/utils/index.ts
@@ -0,0 +1,14 @@
+export * from "./util-hooks";
+export * from "./classnames";
+export * from "./console-styles";
+export * from "./debounce";
+export * from "./debug-time";
+export * from "./delay";
+export * from "./disable-hidden-children";
+export * from "./env-date-formatter";
+export * from "./numbers";
+export * from "./theme-apply";
+export * from "./merge-options";
+export * from "./valtio-array";
+
+// export * from "./uuid"; // use it from manifest
