c4ca77afba5d949b4e8811940938c8cfb05d7f51
Max Zakharzhevskiy <maxzz@msn.com>
2025-11-22 16:12:32 -0800

    WIP. fix build errors

--- src/components/0-all/0-app.tsx
+++ src/components/0-all/0-app.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/shadcn/button";
+
 export function App() {
     return (
         <div className="min-h-screen bg-green-500/50">
-            123
+            <Button>Shadcn Button</Button>
         </div>
     );
 }
--- src/utils/util-hooks/index.tsx
+++ src/utils/util-hooks/index.tsx
@@ -1,14 +1,8 @@
 export * from "./atomsX";
 export * from "./atomWithListeners";
 export * from "./merge-refs";
-export * from "./toasts-next";
 export * from "./use-atom-effect";
 export * from "./use-memo-one";
-export * from "./use-resize-observer";
-export * from "./spy-all-icons";
 export * from "./useCallbackRef";
 export * from "./useClientRect";
 export * from "./useDebouncedValue";
-export * from "./useElementClickAway";
-
-export * from "./toasts-auto-cleanup";
